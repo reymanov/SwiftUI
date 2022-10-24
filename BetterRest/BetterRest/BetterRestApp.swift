409d6772e668f86e2ba533a2d3422fef4c4644d8
Jakub Rejmann <jakub.rejmann@solveq.io>
2022-10-24 17:45:15 +0200

    finish BetterRest app with CoreML features

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kuba Rejmann on 19/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
